6fdf6948129b79af2520f1e84b027c8d744e8c63
Denis Stark <39776471+DenisStark77@users.noreply.github.com>
2023-03-31 17:16:59 -0600

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -4,7 +4,7 @@ import asyncio
 import traceback
 import functions_framework
 from telegram import Bot, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 #bot = Bot(token=os.environ["TELEGRAM_TOKEN"])
 application = ApplicationBuilder().token(os.environ["TELEGRAM_TOKEN"]).build()
@@ -12,7 +12,7 @@ application = ApplicationBuilder().token(os.environ["TELEGRAM_TOKEN"]).build()
 # define command handler
 application.add_handler(CommandHandler("help", help_command_handler))
 # define message handler
-#dispatcher.add_handler(MessageHandler(Filters.text, main_handler))
+#dispatcher.add_handler(MessageHandler(filters.text, main_handler))
 
 async def help_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends explanation on how to use the bot."""
