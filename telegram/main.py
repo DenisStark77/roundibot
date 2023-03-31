85ab222c95d9c2a346e9e21e5eadde249dd32d81
Denis Stark <39776471+DenisStark77@users.noreply.github.com>
2023-03-31 16:57:35 -0600

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -1,13 +1,22 @@
 import os
+import json
 import asyncio
 import traceback
 import functions_framework
 import telegram
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler
 
 bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+dispatcher = Dispatcher(bot, None, use_context=True)
 
-async def send(chat, msg):
-    return await bot.sendMessage(chat_id=chat, text=msg)
+# define command handler
+dispatcher.add_handler(CommandHandler("help", help_command_handler))
+# define message handler
+#dispatcher.add_handler(MessageHandler(Filters.text, main_handler))
+
+async def help_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends explanation on how to use the bot."""
+    await update.message.reply_text("Use /issue <asset code> <quantity> to issue your tokens")
 
 @functions_framework.http
 def webhook(request):
@@ -23,11 +32,11 @@ def webhook(request):
     try:
         if request.method == "POST":
             update = telegram.Update.de_json(request.get_json(force=True), bot)
-            asyncio.run(send(update.message.chat.id, 'Hello, hello, hello')) # + update.message.text))
-            return ('', 204)
+            dispatcher.process_update(update)
+            return ('', 200)
         else:
             return ('Bad request', 400)
     except Exception as e:
         print('Exception in webhook [%s]' % e)
         traceback.print_exc()
-        return ('Exception', 400)
+        return ('Exception', 500)
