828206075aefafabbc0916fe980a59e04453f97e
Denis Stark <39776471+DenisStark77@users.noreply.github.com>
2023-04-05 09:53:56 -0600

    Create stellar.py

---
+++ telegram/stellar.py
@@ -0,0 +1,45 @@
+import requests
+from decimal import Decimal
+from stellar_sdk import Keypair, Server, Asset, Network, TransactionBuilder, LiquidityPoolAsset
+
+# Initialize Stellar client
+starting_balance = 3 
+stellar_seed = os.getenv('STELLAR_SEED')
+if os.getenv('BOT_ENV') == 'TEST':
+    stellar = Server(horizon_url="https://horizon-testnet.stellar.org")
+    stellar_passphrase = Network.TESTNET_NETWORK_PASSPHRASE
+else:
+    stellar = Server(horizon_url="https://horizon.stellar.org")
+    stellar_passphrase = Network.PUBLIC_NETWORK_PASSPHRASE
+bot_keypair = Keypair.from_secret(stellar_seed)
+bot_public = bot_keypair.public_key
+
+
+# Function to create an account
+def st_create_account():
+    # Generate new account
+    keypair = Keypair.random()
+
+    # Sends funds to source new account
+    bot_account = stellar.load_account(bot_public)
+    transaction = (
+        TransactionBuilder(
+            source_account=bot_account,
+            network_passphrase=network_passphrase,
+            base_fee=100,
+        )
+        .append_payment_op(
+            destination=keypair.public_key,
+            asset='native',
+            amount="%d" % starting_balance,
+        )
+        .set_timeout(100)
+        .build()
+    )
+    transaction.sign(bot_keypair)
+    resp = stellar.submit_transaction(transaction)
+    if resp['successful'] != True:
+        print(f'Initial funds load failed:\n{resp}')
+        return None
+    else:
+        return keypair
