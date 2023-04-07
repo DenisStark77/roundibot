109a9f8933ab0e0e2b498f1eddf49cc61b88a04c
Denis Stark <39776471+DenisStark77@users.noreply.github.com>
2023-04-07 15:34:29 -0600

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -2,9 +2,9 @@ import os
 import json
 import traceback
 import functions_framework
-from telegram import Bot, Update
-from telegram.ext import Dispatcher, Updater, CommandHandler, MessageHandler, filters
-from stellar_sdk import Keypair
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup 
+from telegram.ext import Dispatcher, Updater, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from stellar_sdk import Keypair, Asset
 from stellar import st_create_account, st_issue_asset, st_send
 
 
@@ -15,6 +15,22 @@ users = db.collection(u'users')
 invites = db.collection(u'invites')
 assets = db.collection(u'assets')
 
+
+# Function to check if string is number
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+# Strip leading @ if any
+def strip_user(s):
+    if s[0] == '@':
+        s = s[1:]
+    return s.lower()
+
+    
 # Telegram bot error handler
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -71,7 +87,8 @@ def invite_command_handler(update, context):
         return
 
     username = update.message.from_user.username.lower()
-    ivitee = context.args[0].lower()
+    ivitee = strip_user(context.args[0])
+        
     invites.document(ivitee).set({'invited_by': username})
     update.message.reply_text(f"User @{ivitee} invited. He can start using the bot.")
     bot.send_message(admin_chat_id, f"New user @{ivitee} invited by @{username}")
@@ -160,19 +177,120 @@ def list_command_handler(update, context):
 
 # /trust command wrapper 
 def trust_command_handler(update, context):
-    """Sends explanation on how to use the bot."""
-    #TODO: set trust to the asset
-    #TODO: if user has non zero balances check paths to the new asset and show how to order it
+    """Trust an asset in order to use it."""
+    # Check a sintax
+    if len(context.args) != 1:
+        update.message.reply_text("Syntax: /trust <asset code>") # TODO: Add amount to trust
+        return
     
-    update.message.reply_text("Use /order <amount> <buying asset> <amount> <selling assed> to order these tokens")
+    # Check if user is registered in Firebase
+    uid = f"{update.message.from_user.id}"
+    username = update.message.from_user.username.lower()
+    asset_code = context.args[0]
 
+    user_rec = users.document(uid).get()
+    user_info = user_rec.to_dict()
+    
+    if not user_rec.exists:
+        update.message.reply_text("You are not registered yet. Please use command /start")
+        return
+
+    # Check if asset code is exist
+    asset_rec = assets.document(asset_code).get()
+    if not asset_rec.exists:
+        update.message.reply_text(f"Asset {asset_code} not registered. Please check the list of available assets with the /list command.")
+        return        
+        
+    asset_info = asset_rec.to_dict()
+
+    # Set trust to the asset
+    res = st_trust_asset(Keypair.from_secret(user_info['secret']), asset_code, asset_info['public'])
+    if res:
+        update.message.reply_text(f"Trust line added. You can receive {asset_code} tokens. Ask holder to /send {asset_code} @{username}")
+        # TODO: if user has non zero balances check paths to the new asset and show how to order it
+    else:
+        update.message.reply_text(f"Something went wrong. Please try again later. Admins are informed!")
+        bot.send_message(admin_chat_id, f"User @{username} fail to set a trust line for asset {asset_code}")
+
+    
 # /send command wrapper 
 def send_command_handler(update, context):
-    """Sends explanation on how to use the bot."""
-    #TODO: Send given number of the asset to specified user via path payment
+    """Send tokens to another user."""
+    # Check a sintax
+    if len(context.args) != 3 or not isfloat(context.args[0]):
+        update.message.reply_text("Syntax: /send <amount> <asset code> <username>")
+        return
     
-    update.message.reply_text("Use /order <amount> <buying asset> <amount> <selling assed> to exchange tokens to another token")
+    # Check if user is registered in Firebase
+    uid = f"{update.message.from_user.id}"
+    username = update.message.from_user.username.lower()
+    amount = float(context.args[0])
+    asset_code = context.args[1]
 
+    user_rec = users.document(uid).get()
+    user_info = user_rec.to_dict()
+    
+    if not user_rec.exists:
+        update.message.reply_text("You are not registered yet. Please use command /start")
+        return
+
+    # Check if asset code is exist
+    asset_rec = assets.document(asset_code).get()
+    if not asset_rec.exists:
+        update.message.reply_text(f"Asset {asset_code} not registered. Please check the list of available assets with the /list command.")
+        return        
+        
+    asset_info = asset_rec.to_dict()
+    asset = Asset(asset_code, asset_info['public'])
+
+    # Check if recepient exist and has a trust line for given asset
+    recepient = strip_user(context.args[0])
+    user_ref = users.where('username', '==', recepient).stream()
+    users = [d for d in user_ref]
+
+    if len(users) == 0:
+        update.message.reply_text(f"Recepient user @{recepient} does not exist.")
+        print(f"send_command_handler: Recepient user @{recepient} does not exist.")
+        return
+    elif len(users) > 1:
+        update.message.reply_text(f"Duplicate username @{recepient}. Transfer could not be made.")
+        print(f"send_command_handler: Duplicate username @{recepient}. Transfer could not be made.")
+        return
+
+    recipient_info = users[0].to_dict()
+    
+    # Search available paths to pay given tokens
+    paths = st_paths(user_info['public'], asset, amount):
+      
+    # If no paths available inform users  
+    if len(paths) == 0:
+        update.message.reply_text(f"No conversion path available to asset {asset_code}.") 
+        # TODO: Analize why no paths: zero balance of accounts, no demand for own tokens
+        return
+
+    # If more than 1 path send a menu with choice how to pay
+    keyboard = [[InlineKeyboardButton(f"{p['source_asset_code']} ({p['source_amount']})", callback_data="%d" % i)] for i, p in enumerate(paths)]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(f"Please confirm your asset and amount to pay {amount} to @{recepient}', reply_markup=reply_markup)        
+    
+    #update.message.reply_text("Use /order <amount> <buying asset> <amount> <selling assed> to exchange tokens to another token")
+
+
+# Parse inline callbacks for buttons
+def button_callback_handler(update, context):
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+                              
+    print('DEBUG!!!', query)                          
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    query.edit_message_text(text=f"Selected option: {query.data}")    
+    # Send given number of the tokens to a specified user via path payment
+    
+    
 # /order command wrapper 
 def order_command_handler(update, context):
     """Sends explanation on how to use the bot."""
@@ -237,6 +355,7 @@ dispatcher.add_handler(CommandHandler(["or", "order"], order_command_handler))
 dispatcher.add_handler(CommandHandler(["pa", "pay"], pay_command_handler))
 dispatcher.add_handler(CommandHandler(["ba","balance"], balance_command_handler))
 dispatcher.add_handler(CommandHandler(["bo","book"], book_command_handler))
+dispatcher.add_handler(CallbackQueryHandler(button_callback_handler))
 dispatcher.add_error_handler(error)
 # define message handler
 #dispatcher.add_handler(MessageHandler(filters.text, main_handler))
